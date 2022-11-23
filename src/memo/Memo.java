04ede889cee4a0f16d00bb05ae13b0ddcd76b06f
jihwan <kiang18@naver.com>
2022-11-23 12:57:44 +0900

    1차 점ê±검

--- src/memo/Memo.java
+++ src/memo/Memo.java
@@ -2,4 +2,14 @@ package memo;
 
 public class Memo {
 
+    private String name;
+    private String password;
+    private String content;
+
+    public Memo(String name, String password, String content) {
+        this.name = name;
+        this.password = password;
+        this.content = content;
+    }
+
 }
