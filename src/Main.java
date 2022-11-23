a1ea0163e3398c25cc2040c7e0bd804c8513aca3
jihwan <kiang18@naver.com>
2022-11-23 10:33:02 +0900

    hungry

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("hello World");
+        boolean exit = false;
+        do{
+            System.out.println("원하는 메뉴를 선택해주세요.");
+            Scanner scanner = new Scanner(System.in);
+            int selectMenu = scanner.nextInt();
+            switch (selectMenu){
+                case 1:
+
+
+            }
+
+        }while(!exit);
     }
+
 }
